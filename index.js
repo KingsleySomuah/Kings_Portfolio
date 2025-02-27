5d0f1e0f2c28fb72d9e6c96fd8e82e63613d23ec
kingsley <kingsleysomuah123@gmail.com>
2025-02-27 08:11:29 +0000

    My first portfolio

---
+++ index.js
@@ -0,0 +1,25 @@
+function toggleMenu(){
+    document.getElementById("nav-links").classList.toggle("show")
+}
+
+// names should take an array
+const names = ["kingsley", "somuah", "asante"]
+// start from first name
+let index = 0; 
+
+function animateName(){
+    const nameElement = document.getElementById("animated-name")
+
+    nameElement.style.opacity =0
+
+    setTimeout(() => {
+        nameElement.textContent = names[index];
+
+        nameElement.style.opacity =1;
+
+        index = (index +1) % names.length
+    }, 500);
+
+    setInterval(animateName,5000)
+}
+animateName()
